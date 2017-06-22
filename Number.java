bf1415e79020cb822dfa0a03e4268d73f3b6bb9f
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2017-06-22 15:06:12 +0300

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,16 +1,14 @@
 public class OutputOfNumbers {
-    public static void main(String[] args) {
-        for(int i = 0; i <= 100; i ++){
-            if (i % 3 == 0 && i % 5 == 0)
-               System.out.print("Tutti-Frutti\n");
-            else
-            if (i % 3 == 0 )
-                System.out.print("Tutti\n");
-            else
-            if (i % 5 == 0)
-               System.out.print("Frutti\n");
-            else
-                System.out.println(i);
-        }
+  public static void main (String[] args) {
+    for (int i = 0; i <= 100; i ++) {
+      if (i % 3 == 0 && i % 5 == 0)
+        System.out.print("Tutti-Frutti\n");
+      else if (i % 3 == 0 )
+        System.out.print("Tutti\n");
+      else if (i % 5 == 0)
+        System.out.print("Frutti\n");
+      else
+        System.out.println(i);
     }
+  }
 }
