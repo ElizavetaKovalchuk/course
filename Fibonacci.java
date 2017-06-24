24cdd25f2a236d26ee2531d6af104e809e4a2c54
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2017-06-24 16:49:20 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-  public static void main (String[] args) {
-    int fibSum = 0;
+  public static void main (String[] args) 
     int fib1 = 0;
     int fib2 = 1;
-    System.out.println("Enter any positive number");
-    Scanner number = new Scanner( System.in );
-    int InputNum = number.nextInt();
+    int fibSum = 0;
+    int InputNum = 0;
+    InputNum = Scan(InputNum);
     if (InputNum < 0) {
       System.out.println("You entered a negative number");
     } else {
@@ -17,11 +16,19 @@ public class Fibonacci {
         fib2 = fibSum;
       }
     }
+  
     if (fib_sum == InputNum) {
       System.out.println("It is the Fibonacci number");
     } else {
       System.out.println("It is not the Fibonacci number");
     }
+  }
+  
+public static int Scan(int i) {
+  System.out.println("Enter any positive number");
+    Scanner number = new Scanner( System.in );
+    i = number.nextInt();
+    retun i;
   }
 }   
  
