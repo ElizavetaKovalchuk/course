6143b00ac0dc86cfed67ae6a3f9adaeeec1911af
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2017-06-26 11:18:11 +0300

    Add files via upload

---
+++ Sequence.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Sequence {
+  public static void main (String[] args) {
+     System.out.println("Enter number with a comma");
+     Scanner scan = new Scanner(System.in);
+     String input = scan.nextLine();
+     String[] number = input.split(",");
+     int[] sequence = new int[number.length];
+     for (int i = 0; i < number.length; i++) {
+       try {
+                sequence[i] = Integer.parseInt(number[i]);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+      }   
+      int n = 0;
+      for (int i = 0; i < number.length; i++) {
+        if (number[i] < number[i+1]) {
+        n++;
+        }
+      }
+      if (n == number.length){
+        System.out.println(" It is ascending sequence ");
+      } else {
+        System.out.println(" It is decreasing sequence");
+      }  
+  }
+}           
+    
+    
+
+
+
