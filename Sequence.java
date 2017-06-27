e46dca1404c18df64389b884b61eb1472589426b
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2017-06-27 13:33:29 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,34 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Sequence {
   public static void main (String[] args) {
-     System.out.println("Enter number with a comma");
-     Scanner scan = new Scanner(System.in);
-     String input = scan.nextLine();
-     String[] number = input.split(",");
-     int[] sequence = new int[number.length];
-     for (int i = 0; i < number.length; i++) {
-       try {
-                sequence[i] = Integer.parseInt(number[i]);
-            } catch (NumberFormatException e) {
-                continue;
-            }
-      }   
-      int n = 0;
-      for (int i = 0; i < number.length; i++) {
-        if (number[i] < number[i+1]) {
+    System.out.println("Enter numbers separated by a comma");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    String[] number = input.split(",");
+    int[] sequence = new int[number.length];
+    for (int i = 0; i < number.length; i++) {
+      try {
+            sequence[i] = Integer.parseInt(number[i]);
+          } catch (NumberFormatException e) {
+            continue;
+          }
+    }   
+    int n = 0;
+    for (int i = 0; i < number.length; i++) {
+      if (number[i] < number[i+1]) {
         n++;
-        }
       }
-      if (n == number.length){
-        System.out.println(" It is ascending sequence ");
-      } else {
-        System.out.println(" It is decreasing sequence");
-      }  
+    }
+    if (n == number.length){
+      System.out.println(" It is ascending sequence ");
+    } else {
+      System.out.println(" It is decreasing sequence");
+    }  
   }
 }           
-    
-    
-
-
-
